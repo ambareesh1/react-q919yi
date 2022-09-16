3e55398fe6c44447448832ec18a7009bbc09b5a7
ambareesh1 <ambru333@gmail.com>
2022-09-16 15:39:31 +0530

    1 file modified

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -14,12 +14,16 @@ import {
 } from 'firebase/firestore';
 import { DataService } from '../DataService';
 const Services = (props) => {
-  console.log(props);
   const [edit, setEdit] = useState(false);
   const [inputs, setInputs] = useState({});
   const [descriptions, setDescription] = useState({});
   const [serviceDescription, setServiceDescription] = useState({});
 
+  const [title, setTitle] = useState({});
+  const [desc, setDes] = useState({});
+  const [add, setAdd] = useState(false);
+
+  const serviceCollectionRef = collection(db, 'Service');
   const onEditClick = () => {
     setEdit(!edit);
   };
@@ -61,6 +65,41 @@ const Services = (props) => {
     setDescription((values) => ({ ...values, [name]: value }));
   };
 
+  const addItem = async (event) => {
+    debugger;
+    let item = {
+      name: title,
+      description: desc,
+      category: 'tabs',
+      imgUrl: '',
+      order: props.items.length + 1,
+    };
+
+    await addDoc(serviceCollectionRef, item);
+    props.reload();
+    setAdd(!add);
+    setEdit(false);
+  };
+
+  const cancelItem = () => {
+    setAdd(false);
+  };
+
+  const showAddForm = () => {
+    setAdd(!add);
+  };
+
+  const onChangeDes = (event) => {
+    setDes(event.editor.getData());
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onCancelEdit = (event) => {
+    setEdit(false);
+  };
   return (
     <div className="about-custom" id="about-1">
       <div className="row">
@@ -78,40 +117,89 @@ const Services = (props) => {
             <i className="bi bi-pencil-fill"></i>
           </span>
         </div>
-        {edit ? (
-          <CKEditor
-            activeClass="p10"
-            name={props.serviceDes[0].identifier}
-            initData={props.serviceDes[0].description}
-            onChange={(event) =>
-              onChangeDescription(event, props.serviceDes[0].id)
-            }
-          />
-        ) : (
-          <p
-            className="service-description"
-            dangerouslySetInnerHTML={{
-              __html: props.serviceDes[0].description,
-            }}
-          ></p>
-        )}
+        <div className="d-none d-sm-block">
+          {edit ? (
+            <CKEditor
+              activeClass="p10"
+              name={props.serviceDes[0].identifier}
+              initData={props.serviceDes[0].description}
+              onChange={(event) =>
+                onChangeDescription(event, props.serviceDes[0].id)
+              }
+            />
+          ) : (
+            <p
+              className="service-description"
+              dangerouslySetInnerHTML={{
+                __html: props.serviceDes[0].description,
+              }}
+            ></p>
+          )}
+        </div>
       </div>
 
       <div className="container shadow p-3 mb-5 bg-body rounded">
         <div className="m-2">
           {edit && (
-            <button type="button" class="btn btn-danger float-end">
-              <i class="bi bi-file-plus-fill"></i>
+            <button
+              type="button"
+              class="btn btn-danger float-end"
+              onClick={showAddForm}
+            >
+              <i class="bi bi-file-plus-fill"></i> Add Tabs
             </button>
           )}
-          <form>
-
+          {add && (
+            <form>
+              <div className="m-2">
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label">
+                    Tab Title
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    name="title"
+                    id="title"
+                    placeholder="Enter title here.."
+                    onChange={onChangeTitle}
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleFormControlTextarea1" class="form-label">
+                    Description
+                  </label>
+                  <CKEditor
+                    activeClass="p10"
+                    name="description"
+                    initData="Enter Description here"
+                    onChange={(event) => onChangeDes(event)}
+                  />
+                </div>
+                <div className="d-flex">
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={addItem}
+                  >
+                    Add
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-default"
+                    onClick={cancelItem}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
             </form>
+          )}
           <form onSubmit={handleSubmit}>
             <ul className="nav nav-tabs mb-3" id="pills-tab" role="tablist">
               {props.items.map((item) =>
                 item.order == 1 ? (
-                  <li className="nav-item" role="presentation">
+                  <li className="nav-item active" role="presentation">
                     {edit ? (
                       <input
                         type="text"
@@ -147,7 +235,7 @@ const Services = (props) => {
                       />
                     ) : (
                       <button
-                        className="nav-link active"
+                        className="nav-link"
                         id={item.identifier}
                         data-bs-toggle="pill"
                         data-bs-target={'#' + item.identifier + 'content'}
@@ -218,9 +306,16 @@ const Services = (props) => {
               )}
             </div>
             {edit && (
-              <div className="mt-3">
+              <div className="mt-3 d-flex">
                 <button type="submit" className="btn btn-success">
                   Update
+                </button>
+                <button
+                  type="submit"
+                  className="btn btn-default"
+                  onClick={onCancelEdit}
+                >
+                  Cancel
                 </button>
               </div>
             )}
