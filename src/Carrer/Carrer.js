eec6ece9b01cad1ac27fcf86be640a66c69c7e45
ambareesh1 <ambru333@gmail.com>
2022-09-18 22:36:50 +0530

    1 file modified, 1 added

--- src/Carrer/Carrer.js
+++ src/Carrer/Carrer.js
@@ -1,14 +1,30 @@
 import React, { useState, form, useEffect } from 'react';
+import './Carrer.css';
 const Carrer = () => {
   return (
-    <div class="card text-center mx-auto">
-      <div class="card-header"></div>
-      <div class="card-body">
-        <h5 class="card-title">Page under construction</h5>
-        <p class="card-text"></p>
-        <a href="#" class="btn btn-primary"></a>
+    <div class="card card-img mb-3 mx-auto">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img
+            src="https://i.ibb.co/CV3fsLy/pexels-linda-ellershein-3127880.jpg"
+            class="img-fluid rounded-start"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="card-footer text-muted"></div>
     </div>
   );
 };
