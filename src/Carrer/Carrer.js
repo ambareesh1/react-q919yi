52961a2f8abd0e33bc81b5cf0b2c5815111068c9
ambareesh1 <ambru333@gmail.com>
2022-09-18 22:39:39 +0530

    1 file modified

--- src/Carrer/Carrer.js
+++ src/Carrer/Carrer.js
@@ -2,26 +2,28 @@ import React, { useState, form, useEffect } from 'react';
 import './Carrer.css';
 const Carrer = () => {
   return (
-    <div class="card card-img mb-3 mx-auto">
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img
-            src="https://i.ibb.co/CV3fsLy/pexels-linda-ellershein-3127880.jpg"
-            class="img-fluid rounded-start"
-            alt="..."
-          />
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </p>
+    <div className="mx-auto">
+      <div class="card card-img mb-3">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img
+              src="https://i.ibb.co/CV3fsLy/pexels-linda-ellershein-3127880.jpg"
+              class="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
