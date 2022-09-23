82ae3b9c09e36ff94100d50a4aeafc65ebb38e93
ambareesh1 <ambru333@gmail.com>
2022-09-23 09:15:54 +0530

    2 files modified

--- src/HomeContainer/HomeContainer.js
+++ src/HomeContainer/HomeContainer.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
-
+import './HomeContainer.css';
 const HomeContainer = () => {
   return (
-    <div class="row commercial-bg"><div class="col-lg-6"><div class="m-5"><h1 class="text-color"> Commercial Projects </h1><p class="text-color">Commercial Projects From upscale business complexes, convention centres, auditoriums, IT parks, corporate structures to shopping malls, hyper markets, hospitality, and entertainment complexes, Projects' portfolio boasts of stunning landmark buildings.</p></div></div><div class="col-lg-6"><div class="m-5"><img alt="commercial projects" class="mx-auto d-block custom-box-shadow rounded commercial-img " src="https://i.ibb.co/hLJFhth/p7.jpg"></div></div></div>
+    <div class="row bg-white">
+      <div class="container">
+        <div class="shadow p-3 mb-5 bg-body rounded">
+          <h1 class="header-text"> Shri Sathi Projects </h1>
+          <p class="header-text-description">
+            Commercial Projects From upscale business complexes, convention
+            centres, auditoriums, IT parks, corporate structures to shopping
+            malls, hyper markets, hospitality, and entertainment complexes,
+            Projects' portfolio boasts of stunning landmark buildings.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
