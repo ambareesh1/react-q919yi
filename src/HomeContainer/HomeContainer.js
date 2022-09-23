34f80a548f225cf554926dd7217e7776e4a44ad9
ambareesh1 <ambru333@gmail.com>
2022-09-23 09:19:55 +0530

    1 file modified

--- src/HomeContainer/HomeContainer.js
+++ src/HomeContainer/HomeContainer.js
@@ -4,13 +4,27 @@ const HomeContainer = () => {
   return (
     <div class="row bg-white">
       <div class="container">
-        <div class="shadow p-3 mb-5 bg-body rounded">
-          <h1 class="header-text"> Shri Sathi Projects </h1>
+        <div class="shadow p-3 m-3 bg-body rounded">
+          <h3 class="header-text"> Shri Sathi Projects </h3>
           <p class="header-text-description">
-            Commercial Projects From upscale business complexes, convention
-            centres, auditoriums, IT parks, corporate structures to shopping
-            malls, hyper markets, hospitality, and entertainment complexes,
-            Projects' portfolio boasts of stunning landmark buildings.
+            Synergy infra Consultants was established in 1995 as Electrical
+            consultants by a team of enthusiastic and experienced engineers and
+            grown into a complete MEP ( Mechanical, Electrical & Plumbing )
+            Consultancy Organization with special focus on Energy Conservation,
+            Green Business, Automation Systems and Water conservation,
+            Harvesting / Treatment Studies. Synergy infra is awarded with ISO
+            9001: 2008 for its procedural manner in maintaining the data and the
+            standardized way of doing projects, by maintaining checklists at
+            each and every stage of the project We are thoroughly conversant
+            with the latest national and international codes, practices and also
+            the latest Technical developments.
+          </p>
+          <p class="header-text-description">
+            We carry out the design engineering as per Indian Standard
+            Specifications, Indian Electricity Rules, National Electrical Code
+            and local Electricity Board regulations for Indian projects and
+            ASHRAE, NEC, NFPA, ANSI, ASTM, IEEE, NEMA, IBC, IPC, IMC, TAC etc
+            for international projects.
           </p>
         </div>
       </div>
