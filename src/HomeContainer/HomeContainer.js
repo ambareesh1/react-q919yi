05ecb840303d95582de8d537af7ef3b12413f0eb
ambareesh1 <ambru333@gmail.com>
2022-09-24 12:46:23 +0530

    2 files modified

--- src/HomeContainer/HomeContainer.js
+++ src/HomeContainer/HomeContainer.js
@@ -3,66 +3,35 @@ import './HomeContainer.css';
 const HomeContainer = () => {
   return (
     <div class="row bg-white">
-      <div class="container">
+      <div className="parallax"> </div>
+      <div className="container">
         <div class="row shadow p-3 m-3 bg-body rounded">
-          <div className="col-lg-8">
-            <h3 class="header-text">
-              <span className="our-color">Our</span> Journey
-            </h3>
-            <p class="header-text-description text-break">
-              Shri Sathi Projects was established in 2017 as Electrical
-              consultants by a team of enthusiastic and experienced engineers
-              and grown into a complete MEP ( Mechanical, Electrical & Plumbing
-              ) Consultancy Organization with special focus on Energy
-              Conservation, Green Business, Automation Systems and Water
-              conservation, Harvesting / Treatment Studies. Synergy infra is
-              awarded with ISO 9001: 2008 for its procedural manner in
-              maintaining the data and the standardized way of doing projects,
-              by maintaining checklists at each and every stage of the project
-              We are thoroughly conversant with the latest national and
-              international codes, practices and also the latest Technical
-              developments.
-            </p>
-            <p class="header-text-description text-break">
-              We carry out the design engineering as per Indian Standard
-              Specifications, Indian Electricity Rules, National Electrical Code
-              and local Electricity Board regulations for Indian projects and
-              ASHRAE, NEC, NFPA, ANSI, ASTM, IEEE, NEMA, IBC, IPC, IMC, TAC etc
-              for international projects.
-            </p>
-          </div>
-          <div className="col-lg-4">
-            <div className="row shadow-lg p-3 mb-5 bg-body rounded">
-              <h4 className="icon-contianer-bg"> Services we provide </h4>
-              <div className="row">
-                <div class="d-flex flex-column mb-3">
-                  <div class="p-2">
-                    <i class="bi bi-building biu"></i>
-                    <p className="services-icon-description">
-                      <span className="text-bold"> Electrical & Plumbing </span>
-                    </p>
-                  </div>
-                  <div class="p-2">
-                    <i class="bi bi-lightning-charge-fill biu"></i>
-                    <p className="services-icon-description">
-                      <span className="text-bold"> Electrical & Plumbing </span>
-                    </p>
-                  </div>
-                  <div class="p-2">
-                    <i class="bi bi-nintendo-switch biu"></i>
-                    <p className="services-icon-description">
-                      <span className="text-bold"> Electrical & Plumbing </span>
-                    </p>
-                  </div>
-                  <div class="p-2">
-                    <i class="bi bi-pip-fill biu"></i>
-
-                    <p className="services-icon-description">
-                      <span className="text-bold"> Electrical & Plumbing </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
+          <div className="col-lg-12">
+            <div>
+              <h3 class="header-text">
+                <span className="our-color">Our</span> Journey
+              </h3>
+              <p class="header-text-description text-break">
+                Shri Sathi Projects was established in 2017 as Electrical
+                consultants by a team of enthusiastic and experienced engineers
+                and grown into a complete MEP ( Mechanical, Electrical &
+                Plumbing ) Consultancy Organization with special focus on Energy
+                Conservation, Green Business, Automation Systems and Water
+                conservation, Harvesting / Treatment Studies. Synergy infra is
+                awarded with ISO 9001: 2008 for its procedural manner in
+                maintaining the data and the standardized way of doing projects,
+                by maintaining checklists at each and every stage of the project
+                We are thoroughly conversant with the latest national and
+                international codes, practices and also the latest Technical
+                developments.
+              </p>
+              <p class="header-text-description text-break">
+                We carry out the design engineering as per Indian Standard
+                Specifications, Indian Electricity Rules, National Electrical
+                Code and local Electricity Board regulations for Indian projects
+                and ASHRAE, NEC, NFPA, ANSI, ASTM, IEEE, NEMA, IBC, IPC, IMC,
+                TAC etc for international projects.
+              </p>
             </div>
           </div>
         </div>
